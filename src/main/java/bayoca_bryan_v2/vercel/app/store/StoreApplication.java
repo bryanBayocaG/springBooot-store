dc70b3c4c1ab6fa6887d7aeeea2b9a103ab2e04d
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-07-13 22:56:37 +0800

    f commit

---
+++ src/main/java/bayoca_bryan_v2/vercel/app/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package bayoca_bryan_v2.vercel.app.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bayoca_bryan_v2/vercel/app/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bayoca_bryan_v2.vercel.app.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
